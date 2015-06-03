48eade4ec4db460c877f3b0dc2cabbbe10d2ff93
lonelyrabbitinthedark <lonelyrabbitinthedark@gmail.com>
2015-06-03 17:45:17 +0400

    Data classes

---
+++ SQLCommander/Data/Database.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLCommander.Data
+{
+    public class Database
+    {
+        private readonly string _dataBasePath;
+        private string _dataBaseName;
+        private List<DataTable> _tables; 
+        private List<User> _users;
+
+        public Database(string dataBasePath)
+        {
+            _dataBasePath = dataBasePath;
+            _users = FetchUsers();
+        }
+
+        public List<User> Users
+        {
+            get { return _users; }
+        }
+
+        public string DataBasePath
+        {
+            get { return _dataBasePath; }
+        }
+
+        public string DataBaseName
+        {
+            get
+            {
+                var temp = String.Empty;
+                for (var i = _dataBasePath.Length - 1; i >= 0; i--)
+                {
+                    if (_dataBasePath[i] != '\\')
+                        temp += _dataBasePath[i];
+                    else
+                        break;
+                }
+                for (var i = temp.Length - 1; i >= 0; i--)
+                {
+                    _dataBaseName += temp[i];
+                }
+                return _dataBaseName;
+            }
+        }
+
+        //TODO: Realise method to fetch tables from database
+        private List<DataTable> FetchTables()
+        {
+            _tables = new List<DataTable>();
+            return _tables;
+        }
+
+        //TODO: Realise method to fetch users from database
+        private List<User> FetchUsers()
+        {
+            _users = new List<User>();
+            return _users;
+        }
+    }
+}
---
+++ SQLCommander/Data/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLCommander.Data
+{
+    public class User
+    {
+        private readonly string _name;
+        private readonly string _password;
+        private readonly string[] _rights;
+
+        public User(string name, string[] rights)
+        {
+            _name = name;
+            _password = String.Empty;
+            _rights = rights;
+        }
+
+        public User(string name, string password, string[] rights)
+        {
+            _name = name;
+            _password = password;
+            _rights = rights;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+        }
+
+        public string[] Rights
+        {
+            get { return _rights; }
+        }
+    }
+}
