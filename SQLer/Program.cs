fc797c4de03357d49e109e30c6b356cf680bf4d0
lonelyrabbitinthedark <lonelyrabbitinthedark@gmail.com>
2015-06-03 16:04:28 +0400

    Initial commit

---
+++ SQLer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SQLCommander.SQL;
+
+namespace SQLer
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sqlWorker =
+                new SqlWorker(
+                    new SqlConnector(SqlConnector.DbType.MS_Access, "Connection_String",
+                        new[] {"-firstParam", "-secondParam"}),
+                    new SqlCommand(SqlCommand.CommandType.Select, "SELECT * FROM Table;"));
+
+            Console.WriteLine(sqlWorker.Connector.FinalConnectionString);
+            Console.WriteLine(sqlWorker.Command.CommandText);
+
+            Console.ReadKey();
+        }
+    }
+}
