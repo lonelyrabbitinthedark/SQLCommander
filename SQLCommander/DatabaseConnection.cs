108ede7d74fad533a3ce174f3ce5c43e6ca45de1
lonelyrabbitinthedark <lonelyrabbitinthedark@gmail.com>
2015-06-04 17:40:21 +0400

    Adding Connectors

---
+++ SQLCommander/DatabaseConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLCommander.Data;
+
+namespace SQLCommander
+{
+    public class DatabaseConnection
+    {
+        private Data.Database _dataBase;
+        private string _connectionString;
+        private string[] _connectionArguments;
+
+        public DatabaseConnection(Data.Database dataBase, string[] connectionArguments)
+        {
+            _dataBase = dataBase;
+            _connectionArguments = connectionArguments;
+        }
+
+        private void BuildConnectionString()
+        {
+            switch (_dataBase.DatabaseType)
+            {
+                case DatabaseType.MS_ACCESS:
+                    break;
+            }
+        }
+
+        public Database DataBase
+        {
+            get { return _dataBase; }
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+    }
+}
---
+++ SQLCommander/DatabaseConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLCommander
+{
+    public class DatabaseConnector
+    {
+        private Data.Database _dataBase;
+    }
+}
